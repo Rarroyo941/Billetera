3de6df5f119b972d28fb9f4a014cc869db990870
Rarroyo941 <arroyo.rodrigo@outlook.com>
2023-01-18 18:04:34 -0300

    funcionalidad

--- js/js.js
+++ js/js.js
@@ -4,3 +4,22 @@ btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark')
     btnSwitch.classList.toggle('active')
 })
+
+let disponible = document.getElementById('disponible')
+let totalIngresos = document.getElementById('totalIngresos')
+let totalGastos = document.getElementById('totalGastos')
+
+let ingresos = 10000
+let gastos = 9000
+let montoDisponible = ingresos - gastos
+
+disponible.innerText = `$ ${montoDisponible}`
+totalIngresos.innerText = `$ ${ingresos}`
+totalGastos.innerText = `$ ${gastos}`
+
+if (montoDisponible >= 0) {
+    disponible.style.color = `green`
+} else {
+    disponible.style.color = `red`
+}
+
