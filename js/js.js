331f5ffe0069d01ea66932fce3ae53d470a97ed6
Rarroyo941 <arroyo.rodrigo@outlook.com>
2023-01-18 22:34:29 -0300

    movimientos

--- js/js.js
+++ js/js.js
@@ -1,25 +1,31 @@
-const btnSwitch = document.querySelector('#switch')
+/*const btnSwitch = document.querySelector('#switch')
 
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark')
     btnSwitch.classList.toggle('active')
-})
+})*/
 
-let disponible = document.getElementById('disponible')
-let totalIngresos = document.getElementById('totalIngresos')
-let totalGastos = document.getElementById('totalGastos')
+(function load () {
+    const $form = document.querySelector("#jsForm")
+    const contenedorGastos = document.querySelector("#contenedorGastos")
+    function impresionHtml (contenedor, tarea, tarea2) {
+        contenedor.innerHTML += `
+        <li class="imprimir">
+            <span class="tarea">${tarea}</span>
+            <span class="tarea2">$${tarea2}</span>
+            <section class="btnAccion">
+                <i class="fa-solid fa-trash-can"></i>
+            </section>
+        </li>
+        `
+    }
+    $form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        const data = new FormData($form)
+        const gastoTexto = data.get("gastoTexto")
+        const gastoNumero = data.get("gastoNumero")
+        impresionHtml(contenedorGastos, gastoTexto, gastoNumero)
+    })
+})()
 
-let ingresos = 10000
-let gastos = 9000
-let montoDisponible = ingresos - gastos
-
-disponible.innerText = `$ ${montoDisponible}`
-totalIngresos.innerText = `$ ${ingresos}`
-totalGastos.innerText = `$ ${gastos}`
-
-if (montoDisponible >= 0) {
-    disponible.style.color = `green`
-} else {
-    disponible.style.color = `red`
-}
 
